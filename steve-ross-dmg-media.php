4ccc8d00bdb12a850e1800b4f2c3a708a830418f
Steve Ross <steve@ross.wtf>
2025-02-16 19:24:04 +0000

    wp-cli excercise

--- steve-ross-dmg-media.php
+++ steve-ross-dmg-media.php
@@ -24,7 +24,35 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
+
+add_action( 'init', 'dmg_media_postlink' );
 function dmg_media_postlink() {
 	register_block_type( __DIR__ . '/build/postlink' );
 }
-add_action( 'init', 'dmg_media_postlink' );
+
+if(class_exists('WP_CLI')) WP_CLI::add_command( 'dmg-read-more-search', 'DMG_READ_MORE_SEARCH' );
+function DMG_READ_MORE_SEARCH( $args ) {
+	$args = wp_parse_args( $args, []);
+
+	$after = (@$args[0]) ? $args[0] : date("Y-m-d H:i", time() - 30 * DAY_IN_SECONDS);
+	$before = (@$args[1]) ? $args[1] : date("Y-m-d H:i");
+
+	$posts = get_posts([
+		'post_type' => 'ANY',
+		's' => '<!-- wp:dmg-media/postlink ',
+		'date_query' => [
+			'after' => $after,
+			'before' => $before,
+		],
+		'fields' => 'ids',
+	]);
+
+	if(!strtotime($after) || !strtotime($before)):
+		WP_CLI::log("Invalid date(s) provided. Format must be YYYY-MM-DD.");
+	elseif($posts):
+		WP_CLI::log(implode(", ", $posts));
+	else:
+		WP_CLI::log("No posts found between $after and $before.");
+	endif;
+
+}
